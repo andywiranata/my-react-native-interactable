5758bd4b18fd20e17e0eadf0379f1de82acc4d74
andy wiranata <andywiranata@Live.com>
2018-07-26 14:09:05 +0700

    Update Interactable.java

--- android/src/main/java/com/wix/interactable/Interactable.java
+++ android/src/main/java/com/wix/interactable/Interactable.java
@@ -16,7 +16,7 @@ public class Interactable implements ReactPackage {
         return Collections.emptyList();
     }
 
-    @Override
+    // @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
